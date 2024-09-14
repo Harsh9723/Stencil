711843044aaba64f6685f12e10cf4e5e1682a3dc
Harsh <shv245245@gmail.com>
2024-09-14 17:58:16 +0530

    dnd

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,11 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
-    // <TreeDataProvider>
-    // <MemoryRouter>
+    <TreeDataProvider>
+    <MemoryRouter>
       <App />
-    // </MemoryRouter>
-    // </TreeDataProvider>
+     </MemoryRouter>
+     </TreeDataProvider>
   /* </React.StrictMode> */
 );
 
