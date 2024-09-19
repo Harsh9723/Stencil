65a64ed6ba5cd905f00dd9139ccdf62cd67880b4
Harsh <shv245245@gmail.com>
2024-09-19 16:43:36 +0530

    .

--- src/Components/SvgContent.jsx
+++ src/Components/SvgContent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { styled } from '@mui/system';
 import { Card, } from '@mui/material';
 import {insertSvgContentIntoOffice} from '../Common/CommonFunction'
